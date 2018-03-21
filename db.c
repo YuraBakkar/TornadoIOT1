3ec4804652da55ffc76f05cdd1c43110f4b69072
Yura Stepanischev <yura@bakkar.biz>
2018-03-21 13:54:46 +0200

    test2

--- db.c
+++ db.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <mysql.h>
 
+void finish_with_error(MYSQL *con)
+{
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);        
+}
+
 int main(int argc, char **argv){
   MYSQL *con = mysql_init(NULL);
   if (con == NULL){
@@ -9,14 +16,24 @@ int main(int argc, char **argv){
     exit(1);
   }
   if (mysql_real_connect(con, "localhost", "root", "qwopaskl", NULL, 0, NULL, 0) == NULL){
-    fprintf(stderr, "%s\n", mysql_error(con));
-    mysql_close(con);
-    exit(1);
+    finish_with_error(con);
   }
   if (mysql_query(con, "CREATE DATABASE IF NOT EXISTS security")){
-    fprintf(stderr, "%s\n", mysql_error(con));
-    exit(1);
+    finish_with_error(con);
   }
+  
+  if (mysql_query(con, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'security' AND table_name = 'data';")) 
+  {
+      finish_with_error(con);
+  }
+  
+  MYSQL_RES *result = mysql_store_result(con);
+  
+  if (result == NULL) 
+  {
+      finish_with_error(con);
+  }
+  
   mysql_close(con);
   exit(0);
 }
