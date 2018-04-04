ff4c869c8b98ff344f2defa3a854429813e039c0
Yura Stepanischev <yura@bakkar.biz>
2018-04-04 16:21:42 +0300

    333

--- db.c
+++ db.c
@@ -212,13 +212,13 @@ void checkDoors(int d){
       if ( openDoor[d-1] ){
         if (strlen(phone1)){
           sendSMS(d,1);
-          usleep(2000000);
+          sleep(2);
           callPhone(1);//sendSMS(d,1);
           usleep(callDelay*1000000);
         }
         if (strlen(phone2)){
           sendSMS(d,2);
-          usleep(2000000);
+          sleep(2);
           callPhone(2);//sendSMS(d,1);
           usleep(callDelay*1000000);
         }
