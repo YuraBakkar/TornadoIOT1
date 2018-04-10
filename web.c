304fdf0cc7118ce7d53ef7bfd1bb8fc044e2e339
Yura Stepanischev <yura@bakkar.biz>
2018-04-10 10:59:11 +0300

    web2

--- web.c
+++ web.c
@@ -1,13 +1,36 @@
 #include <iostream>
 #include "fcgio.h"
+#include <mysql.h>
 
 using namespace std;
+
+MYSQL *con;
 
 int main(void) {
     // Backup the stdio streambufs
     streambuf * cin_streambuf  = cin.rdbuf();
     streambuf * cout_streambuf = cout.rdbuf();
     streambuf * cerr_streambuf = cerr.rdbuf();
+	
+	//////////////////////////////////////////////////////////////////////////
+	con = mysql_init(NULL);
+	if (con == NULL){
+    fprintf(stderr, "%s\n", mysql_error(con));
+    exit(1);
+	}
+	if (mysql_real_connect(con, "localhost", "root", "qwopaskl", NULL, 0, NULL, 0) == NULL){
+		finish_with_error(con);
+	}
+	if (mysql_query(con, "use security;")){
+		finish_with_error(con);
+	}
+	if (mysql_query(con, "SELECT * FROM log WHERE dat+INTERVAL 7 DAY>NOW();"))
+    finish_with_error(con);
+  
+	mysql_free_result(result);
+	MYSQL_RES *result = mysql_store_result(con);
+	//////////////////////////////////////////////////////////////////////////
+	
 
     FCGX_Request request;
 
@@ -30,8 +53,15 @@ int main(void) {
              << "    <title>Hello, World!</title>\n"
              << "  </head>\n"
              << "  <body>\n"
-             << "    <h1>Hello, World!</h1>\n"
-             << "  </body>\n"
+             << "    <h1>Hello!</h1>\n<table>";
+		MYSQL_ROW row;
+		while ((row = mysql_fetch_row(result))) 
+		{ 
+			cout << "<tr>";
+			cout << "<td>" << row[1] << "</td><td>" << row[2] << "</td>"; 
+			cout << "</tr>\n";
+		}
+		cout << "</table></body>\n"
              << "</html>\n";
 
         // Note: the fcgi_streambuf destructor will auto flush
